18457632f5e90dfd4c87d4767928365eb9e7d2dc
Tim Tang <107114571+ncTimTang@users.noreply.github.com>
2025-03-11 15:40:58 +0800

    init

--- evaluation/scripts/llava_video_7b/videomme_aks_llava_video_7b.sh
+++ evaluation/scripts/llava_video_7b/videomme_aks_llava_video_7b.sh
@@ -16,7 +16,7 @@ python ./evaluation/insert_frame_num.py \
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --num_processes 8 --main_process_port 12345 -m lmms_eval \
     --model llava_vid \
-    --model_args pretrained=./checkpoints/llava_video_7b,device_map=auto,conv_template=chatml_direct,video_decode_backend=decord,max_frames_num=64,overwrite=False,use_topk=True \
+    --model_args pretrained=./checkpoints/llava_video_7b,conv_template=chatml_direct,video_decode_backend=decord,max_frames_num=64,overwrite=False,use_topk=True \
     --tasks videomme \
     --batch_size 1 \
     --log_samples \
